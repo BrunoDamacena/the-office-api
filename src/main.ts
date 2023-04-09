ae2bca5c7ad2edc5c6c59688f110b3ff445dbd19
Bruno Damacena <damacena.bruno@gmail.com>
2023-04-09 20:17:18 -0300

    added swagger on api-docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('The Office API')
+    .setDescription('')
+    .setVersion('1.0')
+    .build();
+
+  const swaggerDocs = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, swaggerDocs);
+
   await app.listen(3000);
 }
 bootstrap();
