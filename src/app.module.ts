99097351a66d8290da4c8ad5062931a6e40a84c6
Bruno Damacena <damacena.bruno@gmail.com>
2023-04-09 21:17:26 -0300

    add database integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DATABASE_HOST'),
+        port: +config.get<number>('DATABASE_PORT'),
+        username: config.get<string>('DATABASE_USER'),
+        password: config.get<string>('DATABASE_PASSWORD'),
+        database: 'theofficeapi',
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      }),
+    }),
+    HealthModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
